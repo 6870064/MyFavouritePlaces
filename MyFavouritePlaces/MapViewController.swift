7946321184460627dcb0f5bbfe2a1a2ea5108c1a
6870064 <6870064@gmail.com>
2019-12-15 22:31:08 +0300

    27. Map View Controller.

---
+++ MyFavouritePlaces/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  MyFavouritePlaces
+//
+//  Created by Dzmitry Viachaslavau on 12/15/19.
+//  Copyright © 2019 Dzmitry Viachaslavau. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeCV() {
+    }
+    
+}
