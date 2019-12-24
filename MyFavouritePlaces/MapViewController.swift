fa7f1d3eca1204a12c89d49009ff3834c54983fa
6870064 <6870064@gmail.com>
2019-12-24 06:06:02 +0300

    34. Get Address from Map

--- MyFavouritePlaces/MapViewController.swift
+++ MyFavouritePlaces/MapViewController.swift
@@ -26,6 +26,8 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        addressLabel.text = ""
         mapView.delegate = self
         setupMapView()
         checkLocationServices()
@@ -130,7 +132,11 @@ class MapViewController: UIViewController {
         }
     }
     
-    
+    private func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+    let longitude = mapView.centerCoordinate.longitude
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
     
     
     private func showAlert(title: String, message: String) {
@@ -163,7 +169,38 @@ extension MapViewController: MKMapViewDelegate {
             return annotationView
         
     }
-}
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+    
+    let center = getCenterLocation(for: mapView)
+    let geocoder = CLGeocoder()
+    
+    geocoder.reverseGeocodeLocation(center) { (placemarks, error) in
+        
+        if let error = error {
+            print(error)
+            return
+        }
+        
+        guard let placemarks = placemarks else { return }
+        
+        let placemark = placemarks.first
+        let streetName = placemark?.thoroughfare
+        let buildNumber = placemark?.subThoroughfare
+            
+        DispatchQueue.main.async {
+            
+            if streetName != nil && buildNumber != nil {
+                                self.addressLabel.text = "\(streetName!), \(buildNumber!)"
+                            } else if streetName != nil {
+                                self.addressLabel.text = "\(streetName!)"
+                            } else {
+                                self.addressLabel.text = ""
+                            }
+                        }
+                    }
+                }
+            }
     
     extension MapViewController: CLLocationManagerDelegate {
         func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
