d065ec0c76e7cd5a24f69eeb504dd454425fc629
6870064 <6870064@gmail.com>
2019-12-29 18:18:25 +0300

    37. Tracking User Location

--- MyFavouritePlaces/MapViewController.swift
+++ MyFavouritePlaces/MapViewController.swift
@@ -21,9 +21,15 @@ class MapViewController: UIViewController {
     var place = Place()
     let annotationIdentifier = "annotationIdentifier"
     let locationManager = CLLocationManager()
-    let regionInMeters = 10_000.00
+    let regionInMeters = 1000.00
     var incomeSegueIdentifier = ""
     var placeCoordinate: CLLocationCoordinate2D?
+    var directionsArray: [MKDirections] = []
+    var previousLocation: CLLocation? {
+        didSet {
+            startTrackingUserLocation()
+        }
+    }
     
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var mapPinImage: UIImageView!
@@ -70,6 +76,13 @@ class MapViewController: UIViewController {
             doneButton.isHidden = true
             goButton.isHidden = false
         }
+    }
+    
+    private func resetMapView(withNew directions: MKDirections) {
+        mapView.removeOverlays(mapView.overlays)
+        directionsArray.append(directions)
+        let _ = directionsArray.map { $0.cancel()}
+        directionsArray.removeAll()
     }
     
     private func setupPlaceMark() {
@@ -151,11 +164,26 @@ class MapViewController: UIViewController {
         }
     }
     
+    private func startTrackingUserLocation() {
+        
+        guard let previousLocation = previousLocation else { return }
+        let center = getCenterLocation(for: mapView)
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+        self.showUserLocation()
+    }
+    }
+    
     private func getDirections() {
         guard let location = locationManager.location?.coordinate else {
             showAlert(title: "Error", message: "Current location is not determined")
             return
         }
+        
+        locationManager.startUpdatingLocation()
+        previousLocation = CLLocation(latitude: location.latitude, longitude: location.longitude)
         
         guard let request = createDirectionsRequest(from: location) else {
             showAlert(title: "Error", message: "Destination is not determened")
@@ -163,6 +191,8 @@ class MapViewController: UIViewController {
         }
         
         let directions = MKDirections(request: request)
+        resetMapView(withNew: directions)
+        
         directions.calculate { (response, error) in
             
             if let error = error {
@@ -246,6 +276,14 @@ extension MapViewController: MKMapViewDelegate {
     let center = getCenterLocation(for: mapView)
     let geocoder = CLGeocoder()
     
+        if incomeSegueIdentifier == "showPlace" && previousLocation != nil {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.showUserLocation()
+            }
+        }
+        
+        geocoder.cancelGeocode()
+        
     geocoder.reverseGeocodeLocation(center) { (placemarks, error) in
         
         if let error = error {
